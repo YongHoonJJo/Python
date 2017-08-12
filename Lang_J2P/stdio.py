b5fa513793943f9ff30fe803f7b7ca12b8471fc3
ychooni <starctak@gmail.com>
2017-08-12 16:33:32 +0900

    Program Input / Output

---
+++ Lang_J2P/stdio.py
@@ -0,0 +1,19 @@
+### User Input / Output  ###
+
+# a = raw_input() for python2.7
+a = input() 
+print(a)
+
+# input with prompt msg
+a = input("Input num: ")
+print(a)
+
+print("Life" "is" "too short") # "Lifeistoo short"
+print("Life"+"is"+"too short") # same
+
+print("Life", "is", "too short") # "Life is too short"
+
+# print one line.
+for i in range(10):
+	print(i, end=' ')
+
