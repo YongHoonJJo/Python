d6d0326127babc53b5e7a7ab0b39f198f19fcfb0
ychooni <starctak@gmail.com>
2017-08-13 22:41:38 +0900

    Byte of Python 2.7 // Flow Control

---
+++ ByteOfPython/FlowControl/while.py
@@ -0,0 +1,15 @@
+number = 23
+running = True
+
+while running :
+	guess = int(raw_input('Enter the integer: '))
+	
+	if guess == 23:
+		print 'OK'
+		running = False
+	elif guess < 23:
+		print 'UP'
+	else :
+		print 'DOWN'
+else:
+	print 'while done'
